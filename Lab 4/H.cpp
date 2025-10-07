8513faee644e2f0a06a1c2d9792ae4b715b2119d
Sanzhar <omarkhanovsanzh@gmail.com>
2025-10-07 22:34:17 +0500

    Greater Sum Tree

--- Lab 4/H.cpp
+++ Lab 4/H.cpp
@@ -16,7 +16,35 @@ void insert(Node* &root, int val){
         else insert(root->left, val);
     }
 }
+/*
+Given the root of a binary search tree with distinct keys.
+Replace the key of each node with the sum of the keys over
+the nodes that has greater than or equal key. 
+Print new keys in increasing order.
 
+As a reminder, a binary search tree is a tree
+that satisfies these constraints:
+
+The left subtree of a node contains only nodes
+with keys less than the node’s key.
+
+The right subtree of a node contains only
+nodes with keys greater than the node’s key.
+
+Both the left and right subtrees must also be
+binary search trees.
+
+reverse in order traversal
+*/
+int sum = 0;
+
+void inorder(Node* root){
+    if(root == NULL) return;
+    inorder(root->right);
+    sum += root->data;
+    cout << sum << " ";
+    inorder(root->left);
+}
 int main(){
     int n, x;
     cin >> n;
@@ -26,5 +54,6 @@ int main(){
         insert(root, x);
     }
 
+    inorder(root);
     
 }
