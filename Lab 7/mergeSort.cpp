4aa1ea208192feeb590af70c84f9021cab5308a3
Sanzhar <omarkhanovsanzh@gmail.com>
2025-10-23 22:31:18 +0500

    Merge sort ALGO

--- Lab 7/mergeSort.cpp
+++ Lab 7/mergeSort.cpp
@@ -1,36 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void merge(vector<int> &arr,int l, int mid, int r){
-    int n1 = mid - l + 1;
-    int n2 = r - mid;
+void merge(vector<int> &arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
 
+    // Creating temp vectors
     vector<int> L(n1), R(n2);
-    for(int i = 0; i < n1; i++) L[i] = arr[l + i];
-    for(int i = 0; i < n2; i++) R[i] = arr[mid + 1 + i];
 
-    int i = 0, j = 0, k = l;
+
+    // Copy data to temp vectors L[] and R[]
+    for(int i = 0; i < n1; i++) L[i] = arr[left + i];
+
+    for(int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    // Merging back to arr[left ... right]
     while(i < n1 && j < n2){
-        if(i < n1 && j < n2){
-            if(L[i] <= R[j]) arr[k++] = L[i++];
-            else arr[k++] = R[j++];
-        } 
+        if(L[i] <= R[j]) arr[k++] = L[i++];
+        else arr[k++] = R[j++];
     }
-    while(i < n1) arr[k++] = L[i++];
-    while(j < n2) arr[k++] = R[j++];
+
+    // Copying the rest of elements
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
 }
 
-void mergeSort(vector<int> &arr, int l, int r) {
-    if(l >= r) return;
-    int mid = l + (r-l) / 2;
-    mergeSort(arr, l, mid);
-    mergeSort(arr, mid+1, r);
-    merge(arr, l, mid, r);
+void mergeSort(vector<int> &arr, int left, int right){
+    if(left >= right) return;
+
+    int mid = left + (right - left) / 2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
 }
 
 int main() {
-    vector<int> arr = {8, 3, 5, 1, 4, 7, 2, 6};
+    vector<int> arr = {5, 2, 4, 6, 1, 3};
     mergeSort(arr, 0, arr.size() - 1);
 
-    for(int x : arr) cout << x << " ";
+    for (int x : arr) cout << x << " ";
+    return 0;
 }
