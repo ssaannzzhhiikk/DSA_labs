0e6eaae5ea6009657659734f3341a199e4c50e89
Sanzhar <omarkhanovsanzh@gmail.com>
2025-11-20 00:30:57 +0500

    ms

---
+++ quiz 3/mergeSort.cpp
@@ -0,0 +1,47 @@
+/*
+Complexity Analysis of Merge Sort
+Time Complexity:
+
+Best Case: O(n log n), When the array is already sorted or nearly sorted.
+Average Case: O(n log n), When the array is randomly ordered.
+Worst Case: O(n log n), When the array is sorted in reverse order.
+Auxiliary Space: O(n), Additional space is required for the temporary array used during merging.
+
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void merge(vector<int> &arr, int left, int mid, int right){
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    vector<int> L(n1), R(n2);
+
+    for(int i = 0; i < n1; i++) L[i] = arr[left + i];
+    for(int i = 0; i < n2; i++) R[i] = arr[mid + i + 1];
+
+    int i = 0, j = 0, k = left;
+
+    while(i < n1 && j < n2){
+        if(L[i] <= R[j]) arr[k++] = L[i++];
+        else arr[k++] = R[j++];
+    }
+
+    while(i < n1) arr[k++] = L[i++];
+    while(j < n2) arr[k++] = R[j++];
+}
+
+void mergeSort(vector<int> &arr, int left, int right){
+    if(left >= right) return;
+
+    int mid = left + (right - left) / 2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
+}
+
+// arr, 0, size - 1
