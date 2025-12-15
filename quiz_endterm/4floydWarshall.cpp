5babf8e9b7aa876ac009cce87898b9d3113fba7a
Sanzhar <omarkhanovsanzh@gmail.com>
2025-12-16 01:16:29 +0500

    Correct algo

--- quiz_endterm/4floydWarshall.cpp
+++ quiz_endterm/4floydWarshall.cpp
@@ -1,18 +1,14 @@
-#include <iostream>
-#include <vector>
-#include <climits>
+#include <bits/stdc++.h>
 using namespace std;
 
-void floydWarshall(vector<vector<int>> &dist) {
+void floydWarshall(vector<vector<int>> &dist){
     int V = dist.size();
     const int INF = 1e8;
 
-    // Теперь порядок циклов i → j → k
-    for (int i = 0; i < V; i++) {             // фиксируем источник
-        for (int j = 0; j < V; j++) {         // фиксируем получателя
-            for (int k = 0; k < V; k++) {     // пробуем разные промежуточные вершины
-
-                if (dist[i][k] != INF && dist[k][j] != INF) {
+    for(int k = 0; k < V; k++){
+        for(int i = 0; i < V; i++){
+            for(int j = 0; j < V; j++){
+                if(dist[i][k] != INF && dist[k][j] != INF){
                     dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
